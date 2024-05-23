59e74bfec77a355d5828f0415d2ee6559a49b0e2
mm167 <mm167@example.com>
2024-05-23 17:46:49 +0200

    Initial commit

---
+++ src/main/java/com/formation/DemoBootApplication.java
@@ -0,0 +1,21 @@
+package com.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public  String hello() {
+		return "<h2> Hello BCEAO V1 </h2>";
+	}
+
+}
